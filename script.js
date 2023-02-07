b79b5dab12ad3350d1fc2ef2532c7d907fee74d8
Dianna <dianna@10web.io>
2023-02-07 16:25:26 +0400

    make top bar dismissible

--- script.js
+++ script.js
@@ -708,6 +708,21 @@ function copyToClipboard(element) {
 
 $(document).ready(function() {
 
+    /*TOp bar*/
+    if (typeof jQuery.cookie("closeTopBar") === "undefined") {
+        jQuery('body').addClass('with-topbar');
+        jQuery('.top-bar-container').removeClass('hidden');
+    }
+    /*Remove topbar*/
+    jQuery(".top-bar-container__close").on('click', function () {
+        jQuery('body,html').removeClass('with-topbar');
+        jQuery('.top-bar-container').remove();
+        jQuery.cookie("closeTopBar", '1', {
+            path: '/',
+            domain: '10web.io'
+        });
+    });
+
     /**
      * Add tracking to hire an expert link
      */

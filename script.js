4b02de7f582bd5873518c0870bb4cfcd311f9689
Dianna <dianna@10web.io>
2023-07-18 14:42:22 +0400

    Sunshine chat

--- script.js
+++ script.js
@@ -858,87 +858,65 @@ $(document).ready(function() {
     /**
      * Zendesk chat
      * */
-    let dbUserSubscrCategory = getCookie( 'db-user-subscr-category' );
-    if (dbUserSubscrCategory && dbUserSubscrCategory !== 'starter') {
-        setTimeout(function () {
-            $("#advanced_chat").fadeIn('slow');
-            $(".chats-container, #advanced_chat").removeAttr("style");
-        }, 100);
-    } else {
-        jQuery('.resource_item.contact_us').hide();
+    const dbUserSubscrCategory = getCookie( 'db-user-subscr-category' );
+    if ($('.resource_item.contact_us').length) {
+        if (dbUserSubscrCategory && dbUserSubscrCategory !== 'starter') {
+            setTimeout(function () {
+                tenweb_init_ZE();
+            }, 100);
+        } else {
+            jQuery('.resource_item.contact_us').hide();
+        }
     }
 
-    $(".resource_item.contact_us, .advanced_chat-bubble").on('click', function(){
-        if( typeof zE != 'undefined' && zE('webWidget:get', 'chat:isChatting') ) {
-            zE('webWidget', 'show');
-            zE('webWidget', 'open');
-        }
-        else {
-            $(".chats-chat-name").toggleClass('chats-chat-name-opened');
-            tenweb_openZEChat();
-        }
-
+    $(".resource_item.contact_us").on('click', function(){
+        window['Smooch'].open();
         return false;
     });
 
-
-    function tenweb_openZEChat() {
-        if ( jQuery( '#ze-snippet' ).length === 0 ) {
-            tenweb_init_ZE();
-        }
-        else {
-            if ( typeof zE != 'undefined' ) {
-                zE('webWidget', 'show');
-                zE('webWidget', 'open');
-            }
-        }
-    }
-
-    function openZEChatCallback() {
-        $("#advanced_chat-bubble").removeClass( 'hidden' );
-        zE('webWidget:on', 'chat:unreadMessages', function(number) {
-            if (number > 0) {
-                zE('webWidget', 'show');
-                zE('webWidget', 'open');
-            }
-        });
-        zE('webWidget:on', 'open', function() {
-            $('#advanced_chat').hide();
-        });
-        zE('webWidget:on', 'close', function() {
-            zE('webWidget', 'hide');
-            $('#advanced_chat').show();
-        });
-        zE('webWidget:on', 'chat:connected', function() {
-            zE('webWidget', 'show');
-            zE('webWidget', 'open');
-        });
-        zE('webWidget', 'setLocale', 'en');
-    }
-
     function tenweb_init_ZE() {
-        if ( jQuery( '#ze-snippet' ).length === 0) {
-            tenweb_loadScript( 'https://static.zdassets.com/ekr/snippet.js?key=1b7006ee-2237-41f9-a143-958f11bc68b5', 'ze-snippet', function () {
-                window.zESettings = {
-                    webWidget: {
-                        chat: {
-                            suppress: false,
-                            tags: ['generic_support']
-                        }
-                    }
-                };
-                openZEChatCallback();
-            } );
+        if ( jQuery( '#smooch-snippet' ).length === 0) {
+            tenweb_loadScript( 'smooch-snippet' );
+            Smooch.init({
+                integrationId: "62e93d22b600c900ef13330f",
+                businessName: "10Web",
+                customColors: {
+                    brandColor: "2A2F30",
+                    conversationColor: "2A2F30",
+                    actionColor: "2A2F30",
+                },
+                businessIconUrl: "https://10-web-for-upload.s3.eu-central-1.amazonaws.com/dashboard/10Web-logo-n.png",
+                buttonIconUrl: "https://10-web-for-upload.s3.eu-central-1.amazonaws.com/dashboard/chat-logo.png",
+                buttonWidth: "66",
+                buttonHeight: "64"
+
+            }).then(() => {
+
+            });
         }
     }
 
-    function tenweb_loadScript( src, id, callback ) {
-        if ( $( '#' + id ).length == 0 ) {
-            let script = document.createElement( 'script' );
-            script.setAttribute( 'src', src );
-            script.setAttribute( 'id', id );
-            script.addEventListener( 'load', callback );
-            document.head.appendChild( script );
+    function tenweb_loadScript( id ) {
+        if (jQuery('#' + id).length === 0) {
+            let script = document.createElement('script');
+            script.setAttribute('id', id);
+            script.innerHTML = `!function(o,d,s,e,f){var i,a,p,c=[],h=[];function t(){
+              var t="You must provide a supported major version.";try{if(!f)throw new Error(t);
+              var e,n="https://cdn.smooch.io/",r="smooch";
+              e="string"==typeof this.response?JSON.parse(this.response):this.response;
+              var o=f.match(/([0-9]+)\\.?([0-9]+)?\\.?([0-9]+)?/),s=o&&o[1],i=o&&o[2],a=o&&o[3],p=e["v"+s],c=e["v"+s+"."+i+".patch"];
+              if(e.url||p||c){var h=d.getElementsByTagName("script")[0],u=d.createElement("script");
+              if(u.async=!0,a)u.src=c||n+r+"."+f+".min.js";else{if(!(5<=s&&p))throw new Error(t);
+              u.src=p}h.parentNode.insertBefore(u,h)}}catch(e){e.message===t&&console.error(e)}}
+              o[s]={init:function(){i=arguments;var t={then:function(e){return h.push({type:"t",next:e}),t},catch:function(e){
+              return h.push({type:"c",next:e}),t}};return t},on:function(){c.push(arguments)},render:function(){
+              a=arguments},destroy:function(){p=arguments}},o.__onWebMessengerHostReady__=function(e){
+              if(delete o.__onWebMessengerHostReady__,o[s]=e,i)for(var t=e.init.apply(e,i),n=0;n<h.length;n++){
+              var r=h[n];t="t"===r.type?t.then(r.next):t.catch(r.next)}a&&e.render.apply(e,a),p&&e.destroy.apply(e,p);
+              for(n=0;n<c.length;n++)e.on.apply(e,c[n])};var n=new XMLHttpRequest;
+              n.addEventListener("load",t),n.open("GET","https://"+e+".webloader.smooch.io/",!0),n.responseType="json",n.send()
+              }(window,document,"Smooch","62e93d22b600c900ef13330f","5");`;
+            document.head.appendChild(script);
         }
     }
 

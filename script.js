060c65574e63991cfdc4ea7c780ac7e65e135d0b
Dianna Arakelyan <dianna@10web.io>
2024-08-19 16:48:19 +0400

    Added: target blank to content links

--- script.js
+++ script.js
@@ -1246,6 +1246,10 @@ $(window).on("resize", function () {
     }
 });
 
+function isValidUrl(_string) {
+    const matchpattern = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/gm;
+    return matchpattern.test(_string);
+}
 function getCookie(name) {
     var name = name + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
